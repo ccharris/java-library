7075b379c66ba835402e6c1c8303ea730d25ef03
ccharris <ccharris27@gmail.com>
2016-09-26 10:42:29 -0400

    updated notes

--- src/_02_Hello_World/HelloWorld.java
+++ src/_02_Hello_World/HelloWorld.java
@@ -1,8 +1,9 @@
 package _02_Hello_World;
 
 public class HelloWorld {
-	public static void main(String[] args)
+	public static void main(String[] args) 
+
 	{
-		System.out.println("Hello World");
+		System.out.print("Hello World");
 	}
 }
